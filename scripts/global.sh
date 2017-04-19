0bbb7680ee6474dd3185de09fb291e490afeb1da
Alvaro Miranda Aguilera <kikitux@gmail.com>
2017-04-19 14:13:57 +0200

    add vault
    download vault
    
    ensure ruby is also installed

--- scripts/global.sh
+++ scripts/global.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
 echo "running global provisioning"
+
+which wget ruby &>/dev/null || {
+  apt-get update
+  apt-get install -y wget ruby
+}
+mkdir -p /vagrant/files
+pushd /vagrant/files
+[ -f vault_0.7.0_linux_amd64.zip ] || wget -k https://releases.hashicorp.com/vault/0.7.0/vault_0.7.0_linux_amd64.zip
+
+mkdir -p /usr/local/bin
+pushd /usr/local/bin
+unzip -n /vagrant/files/vault_0.7.0_linux_amd64.zip
+hash -r
+vault -version
+
+gem install rubyzip

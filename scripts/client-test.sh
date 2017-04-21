4e381616f749c5ef0df6538f1f632c36ac24b532
Alvaro Miranda Aguilera <kikitux@gmail.com>
2017-04-21 13:47:05 +0200

    add some tests for client, to ensure we don't break the progress done

---
+++ scripts/client-test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+# all the test must pass
+
+echo "vault cli"
+VAULT_ADDR='http://192.168.56.11:8200' vault read -format=json secret/hello
+VAULT_ADDR='http://vault.service.consul:8200' vault read -format=json secret/hello

457559131db091c8085bfe3b0afa7ca04d2028ee
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-11-13 13:43:08 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -7,29 +7,37 @@
 # - connect to vault
 # - read and print the value of an existing secret (secret/hello)
 
+# load libraries
 require "vault"
 require "mysql"
 
+# load vault-token
 vaulttokenfile = File.read '/vagrant/vault-token'
 vaulttoken = vaulttokenfile.tr("\n","")
 
+# create new vault client
 client = Vault::Client.new(
     address: "http://vault.service.consul:8200",
     token: vaulttoken
 )
 
+# read vault kv kv/mysql
 mysecret = client.logical.read("kv/mysqluser")
 
+# run query on database
 begin
+    # create connection to mysql db using password from vault
     con = Mysql.new 'mydb.mysql.service.consul', 'user', mysecret.data[:value], 'mydb'
     puts con.get_server_info
     rs = con.query 'SELECT count(*) FROM mytable'
     puts rs.fetch_row    
     
+# in case of errors
 rescue Mysql::Error => e
     puts e.errno
     puts e.error
-    
+
+# close connection
 ensure
     con.close if con
 end
